75038d1abb7801d356b1202919bd3a2b51fbf3b7
sonda-p <sondaproyecto@gmail.com>
2024-12-26 21:10:35 -0300

    Update config.js

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "4gjjbj52rnrriqh80ho9abp9hb",     // CognitoClientID
-  "api_base_url": "https://qtx140sfii.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "api_base_url": "https://qtx140sfii.execute-api.us-east-1.amazonaws.com/",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-test-demo.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d3a0j5tgleudr.amplifyapp.com"                                      // AmplifyURL
 };
